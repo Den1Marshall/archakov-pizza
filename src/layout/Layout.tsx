53252060a9279d11be07541e22754f4a10cba4ba
Den Marshall <dank03052005@gmail.com>
2023-05-12 23:51:07 +0300

    added: Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,8 +3,9 @@ import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import SearchContextProvider from '../context/SearchContext';
 import theme from '../utils/theme';
+import { FC } from 'react';
 
-const Layout = () => {
+const Layout: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container fixed>
